1dd364335a2443391d26189944b0330b35196559
William Yeung <williamyeung1141998@gmail.com>
2020-07-21 22:59:38 -0400

    First commit with basic folder setup and extension

---
+++ pokeChat/MessageVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokeChat
+//
+//  Created by William Yeung on 7/21/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class MessageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
